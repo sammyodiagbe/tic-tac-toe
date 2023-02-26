d120e2e484cd1e9d433af0facf7edcd3a88f69b8
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-26 11:36:03 -0600

    styles

--- src/screens/main_screen.js
+++ src/screens/main_screen.js
@@ -4,15 +4,16 @@ import Logo from "../components/logo";
 const MainScreen = () => {
   return (
     <main className="main-container">
-      <div className="icon-container">
-        <Logo />
-      </div>
-      <div className="pick-player-mark">
-        <h3>Pick Player 1's Mark</h3>
+      <div className="content">
+        <div className="icon-container">
+          <Logo />
+        </div>
+        <div className="pick-player-mark">
+          <h3>Pick Player 1's Mark</h3>
 
-        {/* some toggle to pick between marks */}
-        <div className="marker-toggle-container"></div>
-
+          {/* some toggle to pick between marks */}
+          <div className="marker-toggle-container"></div>
+        </div>
         <div className="action-container">
           <Link to="/game">New Game (Vs Cpu)</Link>
           <Link to="/game">New Game (Vs Player)</Link>
