04fedbeade3d2cd4d8bc62ac1e6c3be83f43afa5
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-26 11:03:30 -0600

    done with structure for main screen

--- src/screens/main_screen.js
+++ src/screens/main_screen.js
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import Logo from "../components/logo";
+
 const MainScreen = () => {
-  return <></>;
+  return (
+    <main className="t-main-container">
+      <div className="icon-container">
+        <Logo />
+      </div>
+      <div className="pick-player-mark">
+        <h3>Pick Player 1's Mark</h3>
+
+        {/* some toggle to pick between marks */}
+        <div className="marker-toggle-container"></div>
+
+        <div className="action-container">
+          <Link to="/game">New Game (Vs Cpu)</Link>
+          <Link to="/game">New Game (Vs Player)</Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default MainScreen;
